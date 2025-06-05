cf10c9787ff29aa447dedf24ea829e98b5f3037b
Amonvix <hspedroso@gmail.com>
2025-06-05 16:40:49 -0300

    Data Science Program ready to deploy. Next step is publish in Render

--- titanic_project/settings.py
+++ titanic_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-^)=rm%zwij-ao^z)s9qa%g+m3-^rij26_%6^cdik+xd9#2=inp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -61,7 +61,7 @@ ROOT_URLCONF = "titanic_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'prediction', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
