eae61e80d7e75e755166daa13cf53236e6fbf5d5
Amonvix <hspedroso@gmail.com>
2025-06-05 17:54:04 -0300

    Fix root route to display HTML template

--- titanic_project/urls.py
+++ titanic_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('/predict/', permanent=False)),  # redireciona /
     path('admin/', admin.site.urls),
     path('', include('prediction.urls')),
+    path('', lambda request: redirect('/predict/', permanent=False)),  # redireciona /
 ]
