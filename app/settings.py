2465f9caa306a83f1b76ad76952e935eade73a97
Amonvix <hspedroso@gmail.com>
2025-10-18 18:11:17 -0300

    fix: remove corsheaders from settings

--- app/settings.py
+++ app/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "prediction",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -53,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
